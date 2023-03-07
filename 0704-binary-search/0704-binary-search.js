ca7056ef777fb960a5f15f99609bdec29bd3b966
Vin Vilasboa <79828415+vinnyvilasboa@users.noreply.github.com>
2023-03-07 17:52:31 -0500

    Time: 65 ms (76.05%), Space: 45.2 MB (17.80%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -3,26 +3,22 @@
  * @param {number} target
  * @return {number}
  */
-
-
-// [-1,0,3,5,9,12]
-// 9
 var search = function(nums, target) {
-    
     let left = 0;
     let right = nums.length - 1;
     
-    while(left <= right){
+    while (left <= right){
         let mid = Math.floor((left + right) / 2);
-        let guess = nums[mid];
         
-        if(guess > target){//move left
-            right = mid - 1;
-        }else if(guess < target){//move right
-            left = mid + 1;
-        }else{
-            return mid //if mid === target
+        if(nums[mid] == target){
+            return mid;
+            
+        }else if(nums[mid] > target){
+            right = mid - 1
+        }else {
+            left = mid + 1
         }
     }
-    return -1;
-}
+    return - 1
+
+};
